794403330d157f621c5d1f91743afa4f685e4ab2
John Doe <asdfas@asdfas.ru>
2019-03-24 10:53:36 +0300

    curl

--- commands/MailerController.php
+++ commands/MailerController.php
@@ -23,7 +23,7 @@ class MailerController extends Controller
 
         $auctions = [];
 
-        $data = \darkdrim\simplehtmldom\SimpleHTMLDom::file_curl_get_html("http://www.icetrade.by/search/auctions?search_text=мясо&search=Найти&zakup_type%5B1%5D=1&zakup_type%5B2%5D=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=&created_to=&request_end_from=&request_end_to=&t%5BTrade%5D=1&t%5BeTrade%5D=1&t%5BsocialOrder%5D=1&t%5BsingleSource%5D=1&t%5BAuction%5D=1&t%5BRequest%5D=1&t%5BcontractingTrades%5D=1&t%5Bnegotiations%5D=1&t%5BOther%5D=1&r%5B1%5D=1&r%5B2%5D=2&r%5B7%5D=7&r%5B3%5D=3&r%5B4%5D=4&r%5B6%5D=6&r%5B5%5D=5&sort=num%3Adesc&sbm=1&onPage=20&p=1");
+        $data = \darkdrim\simplehtmldom\SimpleHTMLDom::file_get_html("http://www.icetrade.by/search/auctions?search_text=мясо&search=Найти&zakup_type%5B1%5D=1&zakup_type%5B2%5D=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=&created_to=&request_end_from=&request_end_to=&t%5BTrade%5D=1&t%5BeTrade%5D=1&t%5BsocialOrder%5D=1&t%5BsingleSource%5D=1&t%5BAuction%5D=1&t%5BRequest%5D=1&t%5BcontractingTrades%5D=1&t%5Bnegotiations%5D=1&t%5BOther%5D=1&r%5B1%5D=1&r%5B2%5D=2&r%5B7%5D=7&r%5B3%5D=3&r%5B4%5D=4&r%5B6%5D=6&r%5B5%5D=5&sort=num%3Adesc&sbm=1&onPage=20&p=1");
         $lastNum = Auctions::findOne(['id' => 1]);
 
         $newNum = $data->find('table#auctions-list tr')[1]->find('td')[3]->innerText();
@@ -32,7 +32,7 @@ class MailerController extends Controller
         if ($newNum != $lastNum) {
             for ($i = 1; $i < (int)$link + 1; $i++) {
 
-                $data2 = \darkdrim\simplehtmldom\SimpleHTMLDom::file_curl_get_html("http://www.icetrade.by/search/auctions?search_text=мясо&search=Найти&zakup_type%5B1%5D=1&zakup_type%5B2%5D=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=&created_to=&request_end_from=&request_end_to=&t%5BTrade%5D=1&t%5BeTrade%5D=1&t%5BsocialOrder%5D=1&t%5BsingleSource%5D=1&t%5BAuction%5D=1&t%5BRequest%5D=1&t%5BcontractingTrades%5D=1&t%5Bnegotiations%5D=1&t%5BOther%5D=1&r%5B1%5D=1&r%5B2%5D=2&r%5B7%5D=7&r%5B3%5D=3&r%5B4%5D=4&r%5B6%5D=6&r%5B5%5D=5&sort=num%3Adesc&sbm=1&onPage=20&p=$i");
+                $data2 = \darkdrim\simplehtmldom\SimpleHTMLDom::file_get_html("http://www.icetrade.by/search/auctions?search_text=мясо&search=Найти&zakup_type%5B1%5D=1&zakup_type%5B2%5D=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=&created_to=&request_end_from=&request_end_to=&t%5BTrade%5D=1&t%5BeTrade%5D=1&t%5BsocialOrder%5D=1&t%5BsingleSource%5D=1&t%5BAuction%5D=1&t%5BRequest%5D=1&t%5BcontractingTrades%5D=1&t%5Bnegotiations%5D=1&t%5BOther%5D=1&r%5B1%5D=1&r%5B2%5D=2&r%5B7%5D=7&r%5B3%5D=3&r%5B4%5D=4&r%5B6%5D=6&r%5B5%5D=5&sort=num%3Adesc&sbm=1&onPage=20&p=$i");
                 $count = 0;
                 foreach ($data2->find('table#auctions-list tr') as $element) {
 
