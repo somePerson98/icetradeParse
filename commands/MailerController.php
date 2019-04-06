e7a1178322ebba66810dc228e5491f68ec0b57bb
some person <dahatsevich2017@yandex.by>
2019-04-06 21:20:32 +0300

    new try to change curl

--- commands/MailerController.php
+++ commands/MailerController.php
@@ -12,7 +12,7 @@ use yii\console\Controller;
 use app\models\Auctions;
 use yii\console\ExitCode;
 use Yii;
-use darkdrim\simplehtmldom\SimpleHTMLDom;
+use darkdrim\simplehtmldom\SimpleHTMLDom as SHD;
 
 
 
@@ -21,18 +21,30 @@ class MailerController extends Controller
 
     public function actionSend(){
 
+        $dataStr = $this->file_get_contents_curl("http://www.icetrade.by/search/auctions?search_text=%D0%BC%D1%8F%D1%81%D0%BE&zakup_type%5B1%5D=1&zakup_type%5B2%5D=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=&created_to=&request_end_from=&request_end_to=&t%5BTrade%5D=1&t%5BeTrade%5D=1&t%5BsocialOrder%5D=1&t%5BsingleSource%5D=1&t%5BAuction%5D=1&t%5BRequest%5D=1&t%5BcontractingTrades%5D=1&t%5Bnegotiations%5D=1&t%5BOther%5D=1&r%5B1%5D=1&r%5B2%5D=2&r%5B7%5D=7&r%5B3%5D=3&r%5B4%5D=4&r%5B6%5D=6&r%5B5%5D=5&sort=num%3Adesc&sbm=1&onPage=20&p=1");
+
         $auctions = [];
 
+
         $data = \darkdrim\simplehtmldom\SimpleHTMLDom::file_get_html("http://www.icetrade.by/search/auctions?search_text=%D0%BC%D1%8F%D1%81%D0%BE&search=%D0%9D%D0%B0%D0%B9%D1%82%D0%B8&zakup_type%5B1%5D=1&zakup_type%5B2%5D=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=&created_to=&request_end_from=&request_end_to=&t%5BTrade%5D=1&t%5BeTrade%5D=1&t%5BsocialOrder%5D=1&t%5BsingleSource%5D=1&t%5BAuction%5D=1&t%5BRequest%5D=1&t%5BcontractingTrades%5D=1&t%5Bnegotiations%5D=1&t%5BOther%5D=1&r%5B1%5D=1&r%5B2%5D=2&r%5B7%5D=7&r%5B3%5D=3&r%5B4%5D=4&r%5B6%5D=6&r%5B5%5D=5&sort=num%3Adesc&sbm=1&onPage=20&p=1");
+
+        $data = SHD::str_get_html($dataStr);
+
         $lastNum = Auctions::findOne(['id' => 1]);
 
         $newNum = $data->find('table#auctions-list tr')[1]->find('td')[3]->innerText();
 
         $link = $data->find('div.paging')[0]->lastChild()->innerText();
-        if ($newNum != $lastNum) {
+
+        if ($newNum != $lastNum->number) {
             for ($i = 1; $i < (int)$link + 1; $i++) {
 
+
                 $data2 = \darkdrim\simplehtmldom\SimpleHTMLDom::file_curl_get_html("http://www.icetrade.by/search/auctions?search_text=%D0%BC%D1%8F%D1%81%D0%BE&search=%D0%9D%D0%B0%D0%B9%D1%82%D0%B8&zakup_type%5B1%5D=1&zakup_type%5B2%5D=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=&created_to=&request_end_from=&request_end_to=&t%5BTrade%5D=1&t%5BeTrade%5D=1&t%5BsocialOrder%5D=1&t%5BsingleSource%5D=1&t%5BAuction%5D=1&t%5BRequest%5D=1&t%5BcontractingTrades%5D=1&t%5Bnegotiations%5D=1&t%5BOther%5D=1&r%5B1%5D=1&r%5B2%5D=2&r%5B7%5D=7&r%5B3%5D=3&r%5B4%5D=4&r%5B6%5D=6&r%5B5%5D=5&sort=num%3Adesc&sbm=1&onPage=20&p=$i");
+
+                $dataStr2 = $this->file_get_contents_curl("http://www.icetrade.by/search/auctions?search_text=%D0%BC%D1%8F%D1%81%D0%BE&zakup_type%5B1%5D=1&zakup_type%5B2%5D=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=&created_to=&request_end_from=&request_end_to=&t%5BTrade%5D=1&t%5BeTrade%5D=1&t%5BsocialOrder%5D=1&t%5BsingleSource%5D=1&t%5BAuction%5D=1&t%5BRequest%5D=1&t%5BcontractingTrades%5D=1&t%5Bnegotiations%5D=1&t%5BOther%5D=1&r%5B1%5D=1&r%5B2%5D=2&r%5B7%5D=7&r%5B3%5D=3&r%5B4%5D=4&r%5B6%5D=6&r%5B5%5D=5&sort=num%3Adesc&sbm=1&onPage=20&p=$i");
+                $data2 = SHD::str_get_html($dataStr2);
+
                 $count = 0;
                 foreach ($data2->find('table#auctions-list tr') as $element) {
 
@@ -46,20 +58,20 @@ class MailerController extends Controller
 
                         array_push($auctions, $element);
 
-                        if ($i == $link){
-                            $this->sendMail(
-                                'body_mail',
-                                $auctions,
-                                'd_rahatsevich@mail.ru',
-                                'test.mailer.php@yandex.by'
-                            );
-
-                            $lastNum->number = $newNum;
-                            $lastNum->save();
-
-
-                            die();
-                        }
+//                        if ($i == $link){
+//                            $this->sendMail(
+//                                'body_mail',
+//                                $auctions,
+//                                'd_rahatsevich@mail.ru',
+//                                'test.mailer.php@yandex.by'
+//                            );
+//
+//                            $lastNum->number = $newNum;
+//                            $lastNum->save();
+//
+//
+//                            die();
+//                        }
 
                     } else {
 
@@ -69,13 +81,6 @@ class MailerController extends Controller
                             'd_rahatsevich@mail.ru',
                             'test.mailer.php@yandex.by'
                         );
-//                        $result = Yii::$app->mailer->compose('body_mail', ['auctions' => $auctions])
-//                            ->setFrom('d_rahatsevich@mail.ru')
-//                            ->setTo('test.mailer.php@yandex.by')
-//                            ->setSubject('Тема сообщения')
-////            ->setTextBody('Текст сообщения')
-////            ->setHtmlBody('<b>qweтекст сообщения в формате HTML</b>')
-//                            ->send();
 
                         $lastNum->number = $newNum;
                         $lastNum->save();
@@ -110,6 +115,33 @@ class MailerController extends Controller
 
 
 
+    }
+
+    public function actionTest(){
+
+        $data = $this->file_get_contents_curl("http://www.icetrade.by/");
+
+//        var_dump($data);
+        $html = SHD::str_get_html($data);
+        echo $html;
+        exit();
+
+
+
+    }
+
+    function file_get_contents_curl($url) {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $data;
     }
 
 
