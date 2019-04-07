978831849d044e75ada821e50cdcea027be84594
root <root@dasha.pavlovna2014.fvds.ru>
2019-04-07 21:22:45 +0300

    start changing darkdrim

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=icetrade',
+    'dsn' => 'mysql:host=localhost;dbname=icetrade',
     'username' => 'root',
-    'password' => '',
+    'password' => '123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
