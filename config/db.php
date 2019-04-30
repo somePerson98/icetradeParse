0a24ea9c0307e210d236886a951f55e7b90eaaf0
root <root@dasha.pavlovna2014.fvds.ru>
2019-04-30 14:18:22 +0300

    try to solve the problem

--- config/db.php
+++ config/db.php
@@ -4,11 +4,8 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=icetrade',
     'username' => 'root',
-<<<<<<< HEAD
     'password' => 'p24v05d08r25',
-=======
     'password' => '',
->>>>>>> 7a1407e5a31dd26d4c440aef566ac45bb0ef5be1
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
