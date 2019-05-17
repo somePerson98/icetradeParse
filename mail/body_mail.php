5e5f083d98e7f5f69738f888df8a8f8c37b59b5f
some person <dahatsevich2017@yandex.by>
2019-05-17 22:42:27 +0300

    painted tr

--- mail/body_mail.php
+++ mail/body_mail.php
@@ -3,6 +3,9 @@
     span.nw{
         white-space: nowrap !important;
     }
+    tr:nth-child(odd){
+        background-color: #e2e3e5;
+    }
 </style>
 <table border="1" id="auctions" style="border-collapse: collapse">
     <?php
