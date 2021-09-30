b8efda95bcc680a3ab5ef7f729ef3fac586c9e03
notReally08 <dasha.pavlovna2014@yandex.ru>
2021-09-30 22:32:08 +0300

    test

--- views/parse/index.php
+++ views/parse/index.php
@@ -1,15 +1,3 @@
-<style>
+<?php
 
-    #auctions-block td.lst.top{
-        padding: 15px;
-    }
-    #auctions-block td.lst.top:nth-child(even){
-        background: #d6d6d6;
-    }
-</style>
-<table border="1" id="auctions-block" style="border-collapse: collapse">
-    <thead><?=$thead?></thead>
-    <?php foreach ($parsed as $auction){?>
-            <tr class="auction-row"><?=$auction['item']?></tr>
-    <?php } ?>
-</table>
+echo "index.php";
