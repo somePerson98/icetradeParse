256a4f0948b861424014cd2f1fcfc052a984cd94
somePerson98 <47223490+somePerson98@users.noreply.github.com>
2021-09-30 20:05:14 +0300

    Update MailerController.php

--- commands/MailerController.php
+++ commands/MailerController.php
@@ -54,6 +54,7 @@ class MailerController extends Controller
             if (! $pageCount) {
                 continue;
             }
+            var_dump(1);
             for ($i = 0; $i < $pageCount; $i++) {
                 $p = $i+1;
                 $currentUrl = "http://www.icetrade.by/search/auctions?search_text=$encodedWord&zakup_type%5B1%5D=1&zakup_type%5B2%5D=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=&created_to=&request_end_from=&request_end_to=&t%5BTrade%5D=1&t%5BeTrade%5D=1&t%5BsocialOrder%5D=1&t%5BsingleSource%5D=1&t%5BAuction%5D=1&t%5BRequest%5D=1&t%5BcontractingTrades%5D=1&t%5Bnegotiations%5D=1&t%5BOther%5D=1&r%5B1%5D=1&r%5B2%5D=2&r%5B7%5D=7&r%5B3%5D=3&r%5B4%5D=4&r%5B6%5D=6&r%5B5%5D=5&sort=num%3Adesc&sbm=1&p=$p&onPage=" . self::SHOW_ITEMS;
