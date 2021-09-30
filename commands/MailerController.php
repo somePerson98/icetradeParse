eba4cd4d383faf809096405cedfa67c686025e83
somePerson98 <47223490+somePerson98@users.noreply.github.com>
2021-09-30 21:02:37 +0300

    Update MailerController.php

--- commands/MailerController.php
+++ commands/MailerController.php
@@ -99,6 +99,7 @@ class MailerController extends Controller
         $data = SimpleHTMLDom::file_curl_get_html($url, 1, 5000);
         $totalStr = $data->find('.total') ? $data->find('.total')[0]->innerText() : false;
         echo $data->find('.total')[0]->innerText();
+        echo $url;
         if (! $totalStr) return false;
         $total = preg_replace("/[^,.0-9]/", '', $totalStr);
 
